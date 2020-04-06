7bfb9cfd8a0d46bd0617ac2613c0137845b3712d
Jason Wallenfang <jason.wallenfang@gmail.com>
2020-04-06 11:03:34 -0500

    part9

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -10,3 +11,16 @@ class UserRegistrationForm(UserCreationForm):
         model = User # the model effected
         fields = ["username", "email", "password1", "password2"] # fields on form and in what order
 
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField(required=True)
+
+    # nested namespace for configurations
+    class Meta:
+        model = User # the model effected
+        fields = ["username", "email"] # fields on form and in what order
+
+class ProfileUpdateForm(forms.ModelForm):
+    # nested namespace for configurations
+    class Meta:
+        model = Profile # the model effected
+        fields = ["image"] # fields on form and in what order
--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 #from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserUpdateForm, ProfileUpdateForm
 #from request import request
 
 # messages statuses can be:
@@ -24,4 +24,23 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, "users/profile.html")
+    if request.method == "POST":
+        # create a new form with the data posted
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()  # saves the user
+            profile_form.save()  # saves the user
+            messages.success(request, f"Account updated.")
+            return redirect("profile")
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        "user_form": user_form,
+        "profile_form" : profile_form
+    }
+
+    return render(request, "users/profile.html", context)
