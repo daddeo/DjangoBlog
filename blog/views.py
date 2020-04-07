18acaa02b9996a00872d728a91da63c11eda522b
Jason Wallenfang <jason.wallenfang@gmail.com>
2020-04-06 19:50:43 -0500

    part10

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,67 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 # . means from the models.py in the current package
 from .models import Post
 
 # from django.http import HttpResponse
 
 # Create your views here.
+# a function based view
 def home(request):
     context = {
         "posts": Post.objects.all()
     }
     return render(request, "blog/home.html", context)
 
+# a class based view
+class PostListView(ListView):
+    model = Post
+
+    # Django is looking for a template file of <app>/<model>_<viewtype>.html
+    # e.g. blog/post_list.html
+    template_name = "blog/home.html"
+    context_object_name = "posts"
+    ordering = ["-posted"]
+
+class PostDetailView(DetailView):
+    model = Post
+    # create html with blob/post_detail.html
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ["title", "content"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ["title", "content"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = "/"
+    # create html with blob/post_detail.html
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
 
 def about(request):
+    # return HttpResponse("<h1>Blog About</h1>")
     return render(request, "blog/about.html", {"title": "About"})
-
-
-#    return HttpResponse("<h1>Blog About</h1>")
